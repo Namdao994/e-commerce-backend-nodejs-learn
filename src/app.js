e381a689cdb90540e9aa6e4b0ee0b1f90b32fdba
Namdao994 <namdao994@gmail.com>
2026-03-06 11:53:15 +0700

    section 2

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const {default: helmet} = require('helmet');
+const compression = require('compression');
+//init middleware
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(compression());
+// app.use(morgan('combined'));
+// app.use(morgan('common'));
+// app.use(morgan('short'));
+// app.use(morgan('tiny'));
+//init db
+
+//init route
+app.get('/', (req, res, next) => {
+  const strCompress = 'Hello fantips';
+  return res.status(200).json({
+    message: 'welcome fantipsjs',
+    metadata: strCompress.repeat(10000),
+  });
+});
+
+//handling error
+
+module.exports = app;
