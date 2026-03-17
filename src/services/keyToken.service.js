c6bcbb8ac4de2c4d0eedc04404f412496de4d87b
Namdao994 <namdao994@gmail.com>
2026-03-17 21:39:02 +0700

    section 6

--- src/services/keyToken.service.js
+++ src/services/keyToken.service.js
@@ -5,7 +5,6 @@ const keyTokenModel = require('../models/keytoken.model');
 class KeyTokenService {
   static createKeyToken = async ({userId, publicKey, privateKey}) => {
     try {
-      // toString là bởi vì publicKey là Buffer, lưu vào trong db sẽ bị lỗi vì không đúng kiểu dữ liệu
       const tokens = await keyTokenModel.create({
         user: userId,
         publicKey,
