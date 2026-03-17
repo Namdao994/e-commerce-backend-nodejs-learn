96c9b3654901ca14582c9d9180638e1e3c692f73
Namdao994 <namdao994@gmail.com>
2026-03-17 22:15:30 +0700

    section 7

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -3,13 +3,9 @@
 const AccessService = require('../services/access.service');
 
 class AccessController {
-  signUp = async (req, res, next) => {
-    try {
-      console.log(`[P]::signUp::`, req.body);
-      return res.status(200).json(await AccessService.signUp(req.body));
-    } catch (error) {
-      next(error);
-    }
+  signUp = async (req, res) => {
+    console.log(`[P]::signUp::`, req.body);
+    return res.status(200).json(await AccessService.signUp(req.body));
   };
 }
 
