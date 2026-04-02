21c600fcaf0d45f32e22dcf4e555592348086768
Namdao994 <namdao994@gmail.com>
2026-04-02 14:22:47 +0700

    section 19

---
+++ src/models/repositories/inventory.repo.js
@@ -0,0 +1,12 @@
+const inventoryModel = require('../inventory.model');
+const {Types} = require('mongoose');
+const insertInventory = async ({productId, shopId, stock, location = 'unknown'}) => {
+  return await inventoryModel.create({
+    inven_productId: productId,
+    inven_stock: stock,
+    inven_shopId: shopId,
+    inven_location: location,
+  });
+};
+
+module.exports = {insertInventory};
